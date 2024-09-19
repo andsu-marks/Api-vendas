8520cb7b04b090bce6522097dbe6cd6cf446fc05
andsu <andsu.marks.r@gmail.com>
2024-09-19 11:36:37 -0300

    up backup

--- src/modules/users/routes/Users.routes.ts
+++ src/modules/users/routes/Users.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
-import UsersController from "../controller/UsersController";
 import { Joi, Segments, celebrate } from "celebrate";
+import multer from "multer";
+import uploadConfig from "@config/upload";
+import UsersController from "../controller/UsersController";
 import isAuthenticated from "@shared/http/middlewares/isAuthenticated";
+import UserAvatarController from "../controller/UserAvatarController";
 
 const usersRouter = Router();
 const usersController = new UsersController();
+const usersAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get("/", isAuthenticated, usersController.index);
+
 usersRouter.post(
   "/",
   celebrate({
@@ -18,5 +25,12 @@ usersRouter.post(
   }),
   usersController.create
 );
+
+usersRouter.patch(
+  "/avatar",
+  isAuthenticated,
+  upload.single("avatar"),
+  usersAvatarController.update,
+)
 
 export default usersRouter;
